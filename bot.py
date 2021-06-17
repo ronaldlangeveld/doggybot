39b54db8e420e5a4a800be8001c195454e79ff27
Ronald Langeveld <kook@Ronalds-MacBook-Pro.local>
2021-06-17 10:14:53 +0200

    addeed var

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
 #!
 
 import logging
-
 from telegram import ReplyKeyboardMarkup, Update, Bot, ParseMode
-
 from telegram.ext import (
     Updater,
     CommandHandler,
@@ -12,7 +10,6 @@ from telegram.ext import (
     ConversationHandler,
     PicklePersistence,
     CallbackContext,
-
 )
 import sys
 import http.client
@@ -40,19 +37,14 @@ updater = Updater(token=TOKEN)
 
 debug = os.environ['DEBUG']
 
+
 if debug == "True":
     chat_id = os.environ['DEBUG_ID']
     interval = 1
-else:
+
+if debug == "False":
     chat_id = os.environ['GROUP_ID']
     interval = 2
-
-
-
-# print(TOKEN)
-
-# environment_vars = os.environ['DEBUG_ID']
-# print(environment_vars)
 
 def SendMessage(message):
     robot.send_message(chat_id=chat_id, text=str(message))
@@ -64,4 +56,13 @@ if __name__ == '__main__':
     globals()[args[1]](*args[2:])
 
 
-# /home/ronald/cron/solar/venv/bin/python3 /home/ronald/cron/solar/outage_bot/inv_status.py
+#CRON STUFF
+
+# 0 6 * * * /home/ronald/cron/dogpoop/doggybot/venv/bin/python3 /home/ronald/cron/dogpoop/doggybot/bot.py SendMessage "Good Morning. I request you take me out if I haven't been out yet. I've been trying to hold all night."
+# 0 7 * * * /home/ronald/cron/dogpoop/doggybot/venv/bin/python3 /home/ronald/cron/dogpoop/doggybot/bot.py SendMessage "Now that I've had my breakfast, please take me out again. 💩"
+# 0 10 * * * /home/ronald/cron/dogpoop/doggybot/venv/bin/python3 /home/ronald/cron/dogpoop/doggybot/bot.py SendMessage "I've been sleeping again till now. Let me go out again for a wee."
+# 0 12 * * * /home/ronald/cron/dogpoop/doggybot/venv/bin/python3 /home/ronald/cron/dogpoop/doggybot/bot.py SendMessage "Maybe a good idea to go stretch my legs outside, some fresh air and a bit of sun."
+# 0 14 * * * /home/ronald/cron/dogpoop/doggybot/venv/bin/python3 /home/ronald/cron/dogpoop/doggybot/bot.py SendMessage "Take me outside quickly before Mom come's home!"
+# 0 16 * * * /home/ronald/cron/dogpoop/doggybot/venv/bin/python3 /home/ronald/cron/dogpoop/doggybot/bot.py SendMessage "If there's no monkeys waiting to kill me outside, please may I go outside?"
+# 0 18 * * * /home/ronald/cron/dogpoop/doggybot/venv/bin/python3 /home/ronald/cron/dogpoop/doggybot/bot.py SendMessage "Okay, I need to go empty my tummy before dinner. Oh yes!"
+# 0 20 * * * /home/ronald/cron/dogpoop/doggybot/venv/bin/python3 /home/ronald/cron/dogpoop/doggybot/bot.py SendMessage "I'm ready to go sleep. Let me out for 15 minutes and then feed me a sausage! Good Night."
